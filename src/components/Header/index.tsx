25de7f3f31610df0a05e066adccfa46cb3b2e559
Márcio Vinícius <marcio.campos@outlook.com>
2023-03-10 18:48:14 -0300

    feat: create header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+function Header() {
+  return (
+    <div>
+      <Head>
+        <title>CoinSynch</title>
+      </Head>
+      <p>Hello world!</p>
+    </div>
+  )
+}
+
+export default Header
