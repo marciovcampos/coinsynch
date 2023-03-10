3379a56391167eb0cbba0644c83f4ba1238073da
Márcio Vinícius <marcio.campos@outlook.com>
2023-03-10 02:09:40 -0300

    Initial commit from Create Next App

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,12 @@
+import * as S from './styles'
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Title>Boilerplate 2</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS e Styled Components
+    </S.Description>
+  </S.Wrapper>
+)
+
+export default Main
