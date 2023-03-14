4cb2d4808e3d315b7ab5cdccd110063641a4b381
Márcio Vinícius <marcio.campos@outlook.com>
2023-03-14 10:09:35 -0300

    feat: creaate top cryptos component

---
+++ src/components/TopCryptos/index.tsx
@@ -0,0 +1,15 @@
+import Container from 'components/Container'
+
+import * as S from './styles'
+
+function TopCryptos() {
+  return (
+    <Container>
+      <S.TopCryptosContainer>
+        <S.Title>Top Cryptos</S.Title>
+      </S.TopCryptosContainer>
+    </Container>
+  )
+}
+
+export default TopCryptos
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import AboutUs from 'components/AboutUs'
 import Footer from 'components/Footer'
 import Header from 'components/Header'
 import HeaderCarousel from 'components/HeaderCarousel'
+import TopCryptos from 'components/TopCryptos'
 import Subscribe from 'components/Subscribe'
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <Header />
       <HeaderCarousel />
       <AboutUs />
+      <TopCryptos />
       <Subscribe />
       <Footer />
     </>
